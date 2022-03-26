f91c3e2b7250906d0d004ffdd8fb0400759cc412
elisa.preynat <elisa.preynat@isen.yncrea.fr>
2022-03-26 19:17:36 +0100

    eleventh Commit

--- app/src/main/java/fr/isen/preynat/androiderestaurant/DetailActivity.kt
+++ app/src/main/java/fr/isen/preynat/androiderestaurant/DetailActivity.kt
@@ -2,6 +2,8 @@ package fr.isen.preynat.androiderestaurant
 
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import fr.isen.preynat.androiderestaurant.databinding.ActivityDetailBinding
 import fr.isen.preynat.androiderestaurant.model.Item
@@ -10,6 +12,12 @@ import fr.isen.preynat.androiderestaurant.model.Item
 class DetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailBinding
+    lateinit var quantity: TextView
+    lateinit var plus: Button
+    lateinit var minus: Button
+
+    var counter: Float = 0F
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -25,8 +33,35 @@ class DetailActivity : AppCompatActivity() {
 
         binding.detailSlider.adapter = carouselAdapter
 
+        plus = findViewById(R.id.plus)
+        minus = findViewById(R.id.minus)
+        quantity = findViewById(R.id.quantity)
+        quantity.text = counter.toString()
+        doTotal(item,counter)
 
+        plus.setOnClickListener {
+            if (counter >= 0) {
+                counter++
+                quantity.text = counter.toString()
+                doTotal(item,counter)
+            }
+        }
+        minus.setOnClickListener {
+            if (counter > 0) {
+                counter--
+                quantity.text = counter.toString()
+                doTotal(item,counter)
+            }
+        }
 
 
     }
+
+    private fun doTotal(item: Item, selected: Float) {
+        val ttlPrix: String = item.prices[0].price
+        val total: Float = ttlPrix.toFloat() * selected
+        val totalString: String = "Total : " + total.toString() + "€"
+        binding.ajout.text = totalString
+    }
+
 }
